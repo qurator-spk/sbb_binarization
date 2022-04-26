82e0783e010661c8505d83d772a49bcf7a7fcb62
vahidrezanezhad <vahid631983@gmail.com>
2022-04-26 13:14:04 +0200

    fixing #30. Directory of model(s) is needed not  model file name.

--- sbb_binarize/cli.py
+++ sbb_binarize/cli.py
@@ -9,7 +9,7 @@ from .sbb_binarize import SbbBinarizer
 @command()
 @version_option()
 @option('--patches/--no-patches', default=True, help='by enabling this parameter you let the model to see the image in patches.')
-@option('--model-dir', '-m', required=True, help='directory containing models for prediction')
+@option('--model-dir', '-m', type=click.Path(exists=True, file_okay=False), required=True, help='directory containing models for prediction')
 @argument('input_image')
 @argument('output_image')
 def main(patches, model_dir, input_image, output_image):
